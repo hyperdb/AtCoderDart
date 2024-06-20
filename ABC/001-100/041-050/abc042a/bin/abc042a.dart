64fc26589ce6a36aca29638825cf01393ec232ff
hyperdb <hyperdb@gmail.com>
2024-06-20 14:47:05 +0900

    🩹「パッケージが見つからない」に対応 - ABC-042-A

--- ABC/001-100/041-050/abc042a/bin/abc042a.dart
+++ ABC/001-100/041-050/abc042a/bin/abc042a.dart
@@ -1,15 +1,23 @@
 import "dart:io";
-import 'package:collection/collection.dart';
 
 List<int> getIntList() {
   var s = stdin.readLineSync();
   return s == null ? [] : s.split(" ").map(int.parse).toList();
 }
 
+bool compareList(List<int> src, List<int> dest) {
+  for (int i = 0; i < src.length; i++) {
+    if (src[i] != dest[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 void main() {
-  List abc = getIntList();
+  List<int> abc = getIntList();
 
   abc.sort((a, b) => a.compareTo(b));
 
-  print(abc.equals([5, 5, 7]) ? 'YES' : 'NO');
+  print(compareList(abc, <int>[5, 5, 7]) ? 'YES' : 'NO');
 }
