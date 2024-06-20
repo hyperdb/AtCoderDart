9f73e3635d584206ff260b004feed2d74774af49
hyperdb <hyperdb@gmail.com>
2024-06-20 14:47:27 +0900

    ✨解答を追加 - ABC-042-C

--- ABC/001-100/041-050/abc042c/bin/abc042c.dart
+++ ABC/001-100/041-050/abc042c/bin/abc042c.dart
@@ -1,5 +1,28 @@
-import 'package:abc042c/abc042c.dart' as abc042c;
+import "dart:io";
 
-void main(List<String> arguments) {
-  print('Hello world: ${abc042c.calculate()}!');
+List<int> getIntList() {
+  var s = stdin.readLineSync();
+  return s == null ? [] : s.split(" ").map(int.parse).toList();
+}
+
+void main() {
+  List nk = getIntList();
+  List d = getIntList();
+
+  for (int i = nk[0]; i < 100000; i++) {
+    int p = i;
+    bool f = true;
+    while (p > 0) {
+      int m = p % 10;
+      if (d.contains(m)) {
+        f = false;
+        break;
+      }
+      p = p ~/ 10;
+    }
+    if (f) {
+      print(i);
+      break;
+    }
+  }
 }
