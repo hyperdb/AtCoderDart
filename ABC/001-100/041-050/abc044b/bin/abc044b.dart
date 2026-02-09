2cfd5ac801e5930f18b4b9113edacf458b9db466
hyperdb <hyperdb@gmail.com>
2026-02-09 19:56:42 +0900

    ✨解答を追加 - ABC-044-B

---
+++ ABC/001-100/041-050/abc044b/bin/abc044b.dart
@@ -0,0 +1,30 @@
+/*
+ * ABC-044 B - 美しい文字列
+ * https://atcoder.jp/contests/abc044/tasks/abc044_b
+ */
+import "dart:io";
+
+String getString() {
+  final String? s = stdin.readLineSync();
+  return s ?? "";
+}
+
+/*
+ * メイン処理
+ */
+void main() {
+  final s = getString();
+  Map<String, int> counter = {};
+
+  for (var char in s.split('')) {
+    counter[char] = (counter[char] ?? 0) + 1;
+  }
+
+  counter.forEach((key, value) {
+    if (value % 2 != 0) {
+      print("No");
+      exit(0);
+    }
+  });
+  print("Yes");
+}
